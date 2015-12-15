9f3a93cd8e7cf702f704e0374c520e0f07abd190
JingAn Chen <lzsoft.cja@gmail.com>
2015-12-15 00:07:29 -0500

    Temp.

--- web/js/search.js
+++ web/js/search.js
@@ -8,8 +8,27 @@
             this.parentNode.setAttribute("data-selected", this.value);
         });
 
+        var dl = document.querySelectorAll("td.doctor"),
+            i = 0;
+
+        for (i = 0; i < dl.length; i += 1) {
+            dl[i].style.backgroundImage = "url('../img/portrait/" + dl[i].getAttribute("data-id") + ".jpg')";
+        }
+
+    }
+
+    function portraitize() {
+
+        var pl = document.querySelectorAll("body > table > tbody > tr > td.portrait"),
+            i = 0;
+
+        for (i = 0; i < pl.length; i += 1) {
+            pl[i].style.backgroundImage = "url(../img/portrait/" + Math.randomRange(1, 15) + ".jpg)";
+        }
+
     }
 
     init();
+    portraitize();
 
 }());
