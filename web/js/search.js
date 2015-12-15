7b8ef144be17fbe4dc3362e064dcc663a9e7be2c
JingAn Chen <lzsoft.cja@gmail.com>
2015-12-15 15:36:57 -0500

    The star filter is done on search.html.

--- web/js/search.js
+++ web/js/search.js
@@ -2,33 +2,178 @@
 
     'use strict';
 
-    function init() {
+    var filter_star = 0,
+        dummy_data = [
+            {
+                id: 1,
+                name: "Jebb Bush",
+                star: 0.9
+            },
+            {
+                id: 2,
+                name: "Janiffer Aniston",
+                star: 1.2
+            },
+            {
+                id: 3,
+                name: "Lucy Chen",
+                star: 3.9
+            },
+            {
+                id: 4,
+                name: "Lawrance Tao",
+                star: 4.1
+            },
+            {
+                id: 5,
+                name: "John Brown",
+                star: 1.2
+            },
+            {
+                id: 6,
+                name: "Maple Xu",
+                star: 3.2
+            },
+            {
+                id: 7,
+                name: "Arianna Sefron",
+                star: 4.2
+            },
+            {
+                id: 8,
+                name: "Lia Tong",
+                star: 4.8
+            },
+            {
+                id: 9,
+                name: "Faye Wong",
+                star: 1.7
+            },
+            {
+                id: 10,
+                name: "Mai Nagaoka",
+                star: 2.2
+            },
+            {
+                id: 11,
+                name: "",
+                star: 5
+            },
+            {
+                id: 12,
+                name: "",
+                star: 3.9
+            },
+            {
+                id: 13,
+                name: "",
+                star: 2.9
+            },
+            {
+                id: 14,
+                name: "",
+                star: 4.2
+            },
+            {
+                id: 15,
+                name: "",
+                star: 4.1
+            }
+        ];
 
-        document.querySelector("#type > select").addEventListener("change", function () {
-            this.parentNode.setAttribute("data-selected", this.value);
-        });
+    function filter(entity) {
+        if (entity.star > filter_star) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-        var dl = document.querySelectorAll("td.doctor"),
-            i = 0;
+    function render() {
 
-        for (i = 0; i < dl.length; i += 1) {
-            dl[i].style.backgroundImage = "url('../img/portrait/" + dl[i].getAttribute("data-id") + ".jpg')";
+        var trl = document.querySelectorAll("tr.entity"),
+            i = 0,
+            counter = 1,
+            tr,
+            td,
+            tr2,
+            tr3,
+            footer;
+
+        for (i = 0; i < trl.length; i += 1) {
+            trl[i].parentNode.removeChild(trl[i]);
+        }
+
+        for (i = 0; i < dummy_data.length; i += 1) {
+            if (filter(dummy_data[i])) {
+                if (counter % 5 === 1) {
+                    tr = document.createElement("tr");
+                    tr.classList.add("entity");
+                    tr.appendChild(document.createElement("td"));
+                    document.querySelector("body > table > tbody").appendChild(tr);
+                    tr2 = document.createElement("tr");
+                    tr2.appendChild(document.createElement("td"));
+                    tr2.appendChild(document.createElement("td"));
+                    tr2.classList.add("entity");
+                    tr3 = document.createElement("tr");
+                    tr3.appendChild(document.createElement("td"));
+                    tr3.appendChild(document.createElement("td"));
+                    tr3.classList.add("entity");
+                    document.querySelector("body > table > tbody").appendChild(tr2);
+                    document.querySelector("body > table > tbody").appendChild(tr3);
+                }
+                td = document.createElement("td");
+                td.setAttribute("colspan", 3);
+                td.setAttribute("rowspan", 3);
+                td.classList.add("doctor");
+                td.style.backgroundImage = "url('../img/portrait/" + dummy_data[i].id + ".jpg')";
+                footer = document.createElement("footer");
+                footer.innerHTML = dummy_data[i].name;
+                td.appendChild(footer);
+                tr.appendChild(td);
+                if (counter % 5 === 0) {
+                    tr.appendChild(document.createElement("td"));
+                }
+                counter += 1;
+            }
+        }
+
+        if ((counter - 1) % 5 !== 0) {
+            tr.appendChild(document.createElement("td"));
+
+            for (i = 0; i < 5 - (counter - 1) % 5; i += 1) {
+                document.querySelector("body > table > tbody > tr.entity:nth-last-of-type(1)").appendChild(document.createElement("td"));
+                document.querySelector("body > table > tbody > tr.entity:nth-last-of-type(1)").appendChild(document.createElement("td"));
+                document.querySelector("body > table > tbody > tr.entity:nth-last-of-type(1)").appendChild(document.createElement("td"));
+                document.querySelector("body > table > tbody > tr.entity:nth-last-of-type(2)").appendChild(document.createElement("td"));
+                document.querySelector("body > table > tbody > tr.entity:nth-last-of-type(2)").appendChild(document.createElement("td"));
+                document.querySelector("body > table > tbody > tr.entity:nth-last-of-type(2)").appendChild(document.createElement("td"));
+                document.querySelector("body > table > tbody > tr.entity:nth-last-of-type(3)").appendChild(document.createElement("td"));
+                document.querySelector("body > table > tbody > tr.entity:nth-last-of-type(3)").appendChild(document.createElement("td"));
+                document.querySelector("body > table > tbody > tr.entity:nth-last-of-type(3)").appendChild(document.createElement("td"));
+            }
         }
 
     }
 
-    function portraitize() {
+    function init() {
 
-        var pl = document.querySelectorAll("body > table > tbody > tr > td.portrait"),
-            i = 0;
+        var i = 0;
 
-        for (i = 0; i < pl.length; i += 1) {
-            pl[i].style.backgroundImage = "url(../img/portrait/" + Math.randomRange(1, 15) + ".jpg)";
-        }
+        document.querySelector("#type > select").addEventListener("change", function () {
+            this.parentNode.setAttribute("data-selected", this.value);
+            render();
+        });
+
+        document.querySelector("#star > select").addEventListener("change", function () {
+            this.parentNode.setAttribute("data-selected", this.value);
+            filter_star = parseInt(this.value, 10);
+            render();
+        });
 
     }
 
     init();
-    portraitize();
+    render();
 
 }());
