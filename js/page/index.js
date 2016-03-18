b2ea0684a0b018869da14e688300becd3ad56af7
JingAn Chen <lzsoft.cja@gmail.com>
2016-03-18 17:13:55 -0400

    Index is done.

--- js/page/index.js
+++ js/page/index.js
@@ -12,41 +12,10 @@
 
     self.addEventListener("hey", function () {
 
-        //portraitize(self);
-
     });
 
-    self.querySelectorAll(":scope > nav > div").addEventListener("click", function () {
-        this.parentNode.querySelector(":scope > .on").classList.remove("on");
-        this.parentNode.querySelector(":scope > .shadow").classList.remove("shadow");
-        this.classList.add("on");
-        this.classList.add("shadow");
-        if (this.classList.contains("doctor")) {
-            self.querySelector(":scope > main > footer").classList.add("doctor");
-            self.querySelector(":scope > main > footer").classList.remove("hospital");
-        } else {
-            self.querySelector(":scope > main > footer").classList.add("hospital");
-            self.querySelector(":scope > main > footer").classList.remove("doctor");
-        }
+    self.querySelector(":scope > article > main > footer").addEventListener("click", function () {
+        window.location.href = "#l=search-doctor";
     });
-
-    /*
-    self.querySelector(":scope > main > input").addEventListener("keyup", function () {
-        if (this.value !== "") {
-            self.querySelector(":scope > main > footer").classList.add("on");
-        } else {
-            self.querySelector(":scope > main > footer").classList.remove("on");
-        }
-    });
-
-    self.querySelector(":scope > main > footer").addEventListener("click", function () {
-        if (self.querySelector(":scope > nav > div.doctor").classList.contains("on")) {
-            location.hash = "#l=search-doctor";
-        } else {
-            location.hash = "#l=search-hospital";
-        }
-
-    });
-    */
 
 }());
