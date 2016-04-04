23d2957e3a6db132193c7710e6ba94c7147253fa
JingAn Chen <lzsoft.cja@gmail.com>
2016-04-03 23:07:11 -0400

    Temp.

--- js/page/search.js
+++ js/page/search.js
@@ -16,16 +16,22 @@
     var current_data_hospital = [];
     var current_data_filter_hospital = [];
 
+    var current_latitude = 0;
+    var current_longitude = 0;
+
     var current_type = "doctor";
 
     self.addEventListener("hey", function () {
         self.querySelectorAll(":scope > aside > div").remove();
         current_data_doctor = Cache.Get("doctor");
+        current_data_hospital = Cache.Get("hospital");
         for (var i = 0; i < current_data_doctor.length; i++) {
             current_data_doctor[i].star_num = ((current_data_doctor[i].star[1] + current_data_doctor[i].star[2] * 2 + current_data_doctor[i].star[3] * 3 + current_data_doctor[i].star[4] * 4 + current_data_doctor[i].star[5] * 5) / (current_data_doctor[i].star[1] + current_data_doctor[i].star[2] + current_data_doctor[i].star[3] + current_data_doctor[i].star[4] + current_data_doctor[i].star[5]) || 0).toFixed(1);
         }
         current_data_filter_doctor = current_data_doctor;
+        current_data_filter_hospital = current_data_hospital;
         filter_doctor();
+        filter_hospital();
     });
 
     self.querySelectorAll(":scope > div > section.doctor").addEventListener("click", function () {
@@ -54,48 +60,78 @@
     self.querySelectorAll(":scope > nav.doctor > main > section.star > select").addEventListener("change", filter_doctor);
     self.querySelectorAll(":scope > nav.doctor > main > section.item > input").addEventListener("keyup", filter_doctor);
     self.querySelectorAll(":scope > nav.doctor > main > section.insurance > input").addEventListener("keyup", filter_doctor);
+    self.querySelectorAll(":scope > nav.doctor > main > section.distance > select").addEventListener("change", filter_doctor);
 
     function name_checker(name, keyword) {
-        var flag = keyword === "";
-        for (var n in name) {
-            if (name[n].indexOf(keyword) > -1) {
-                flag = true;
-                break;
+        if (keyword !== "") {
+            for (var n in name) {
+                if (name[n].indexOf(keyword) > -1) {
+                    return true;
+                }
             }
+        } else {
+            return true;
         }
-        return flag;
+        return false;
+    }
+
+    function item_checker(items, keyword) {
+        if (keyword !== "") {
+            for (var item_id in items) {
+                if (name_checker(Cache.Hash("item", items[item_id]).name, keyword)) {
+                    return true;
+                }
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    function insurance_checker(insurances, keyword) {
+        if (keyword !== "") {
+            for (var insurance_id in insurances) {
+                if (name_checker(Cache.Hash("insurance", insurances[insurance_id]), keyword)) {
+                    return true; // Once matched
+                }
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    function distance_checker(schedules, distance) {
+        if (distance !== 9999) {
+            for (var schedule in schedules) {
+                var hospital = Cache.Hash("hospital", schedules[schedule].hospital_id);
+                if (Math.calculateDistance(current_latitude, current_longitude, parseFloat(hospital.latitude), parseFloat(hospital.longitude)) <= distance) {
+                    return true;
+                }
+            }
+        } else {
+            return true;
+        }
+        return false;
     }
 
     function filter_doctor() {
+        self.scrollTop = 0;
         current_data_filter_doctor = [];
         var name = self.querySelector(":scope > nav.doctor > main > section.name > input").value;
         var star = parseFloat(self.querySelector(":scope > nav.doctor > main > section.star > select").value);
         var item = self.querySelector(":scope > nav.doctor > main > section.item > input").value;
         var insurance = self.querySelector(":scope > nav.doctor > main > section.insurance > input").value;
+        var distance = parseInt(self.querySelector(":scope > nav.doctor > main > section.distance > select > option:checked").value, 10);
+        //
         for (var i = 0; i < current_data_doctor.length; i++) {
             if (name_checker(current_data_doctor[i].name, name)) {
                 if (current_data_doctor[i].star_num >= star) {
-                    var flag_item = item === "";
-                    if (!flag_item) {
-                        for (var item_id in current_data_doctor[i].items) {
-                            if (name_checker(Cache.Hash("item", current_data_doctor[i].items[item_id]).name, item)) {
-                                flag_item = true;
-                                break;
+                    if (item_checker(current_data_doctor[i].items, item)) {
+                        if (insurance_checker(current_data_doctor[i].insurances, insurance)) {
+                            if (distance_checker(current_data_doctor[i].schedules, distance)) {
+                                current_data_filter_doctor.push(current_data_doctor[i]);
                             }
-                        }
-                    }
-                    if (flag_item) {
-                        var flag_insurance = insurance === "";
-                        if (!flag_insurance) {
-                            for (var insurance_id in current_data_doctor[i].insurances) {
-                                if (name_checker(Cache.Hash("insurance", current_data_doctor[i].insurances[insurance_id]), insurance)) {
-                                    flag_insurance = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if (flag_insurance) {
-                            current_data_filter_doctor.push(current_data_doctor[i]);
                         }
                     }
                 }
@@ -107,47 +143,83 @@
     function render_doctor() {
         self.querySelectorAll(":scope > aside.doctor > main").remove();
         for (var i = 0; i < current_data_filter_doctor.length; i++) {
-            holder.innerHTML = self.querySelector(":scope > aside > template").innerHTML;
-            var div = holder.firstElementChild;
+            holder.innerHTML = self.querySelector(":scope > aside.doctor > template").innerHTML;
+            var main = holder.firstElementChild;
             if (current_data_filter_doctor[i].portrait && current_data_filter_doctor[i].portrait.id) {
-                div.querySelector(":scope > nav > svg").style.backgroundImage = "url('" + Api.Storage(current_data_filter_doctor[i].portrait.id, current_data_filter_doctor[i].portrait.extension) + "')";
+                main.querySelector(":scope > nav > svg").style.backgroundImage = "url('" + Api.Storage(current_data_filter_doctor[i].portrait.id, current_data_filter_doctor[i].portrait.extension) + "')";
             }
-            div.querySelector(":scope > header > section.name").innerHTML = "Dr. " + current_data_filter_doctor[i].name.en;
+            main.querySelector(":scope > header > section.name").innerHTML = "Dr. " + current_data_filter_doctor[i].name.en;
             var star = ((current_data_filter_doctor[i].star[1] + current_data_filter_doctor[i].star[2] * 2 + current_data_filter_doctor[i].star[3] * 3 + current_data_filter_doctor[i].star[4] * 4 + current_data_filter_doctor[i].star[5] * 5) / (current_data_filter_doctor[i].star[1] + current_data_filter_doctor[i].star[2] + current_data_filter_doctor[i].star[3] + current_data_filter_doctor[i].star[4] + current_data_filter_doctor[i].star[5]) || 0).toFixed(1);
             star = star <= 0 ? 1 : star;
             if (star > Math.floor(star)) {
-                holder.innerHTML = div.querySelector(":scope > footer > section.star > template.star-empty").innerHTML;
+                holder.innerHTML = main.querySelector(":scope > footer > section.star > template.star-empty").innerHTML;
                 var svg = holder.firstElementChild;
-                div.querySelector(":scope > footer > section.star").appendChild(svg);
+                main.querySelector(":scope > footer > section.star").appendChild(svg);
             }
             for (var j = 0; j < Math.floor(star); j++) {
-                holder.innerHTML = div.querySelector(":scope > footer > section.star > template.star").innerHTML;
+                holder.innerHTML = main.querySelector(":scope > footer > section.star > template.star").innerHTML;
                 var svg = holder.firstElementChild;
-                div.querySelector(":scope > footer > section.star").appendChild(svg);
+                main.querySelector(":scope > footer > section.star").appendChild(svg);
             }
             for (var item_id of current_data_filter_doctor[i].items) {
                 var item = Cache.Hash("item", item_id);
                 var span = document.createElement("span");
                 span.innerHTML = item.name["zh-Hans"];
-                div.querySelector(":scope > footer > section.items").appendChild(span);
+                main.querySelector(":scope > footer > section.items").appendChild(span);
             }
             for (var service_id of current_data_filter_doctor[i].services) {
                 var service = Cache.Hash("service", service_id);
                 var span = document.createElement("span");
                 span.innerHTML = service.name["zh-Hans"];
-                div.querySelector(":scope > footer > section.services").appendChild(span);
+                main.querySelector(":scope > footer > section.services").appendChild(span);
             }
             for (var insurance_id of current_data_filter_doctor[i].insurances) {
                 var insurance = Cache.Hash("insurance", insurance_id);
                 var span = document.createElement("span");
                 span.innerHTML = insurance.provider + "/" + insurance.class + "/" + insurance.subclass;
-                div.querySelector(":scope > footer > section.insurances").appendChild(span);
+                main.querySelector(":scope > footer > section.insurances").appendChild(span);
             }
-            div.querySelector(":scope > header > section.phone").innerHTML = current_data_filter_doctor[i].phone || "没有记录";
-            div.querySelector(":scope > header > section.email").innerHTML = current_data_filter_doctor[i].email || "没有记录";
-            div.querySelector(":scope > a").href = "#l=doctor&id=" + current_data_filter_doctor[i].id;
-            self.querySelector(":scope > aside.doctor").appendChild(div);
+            main.querySelector(":scope > header > section.phone").innerHTML = current_data_filter_doctor[i].phone || "没有记录";
+            main.querySelector(":scope > header > section.email").innerHTML = current_data_filter_doctor[i].email || "没有记录";
+            main.querySelector(":scope > a").href = "#l=doctor&id=" + current_data_filter_doctor[i].id;
+            self.querySelector(":scope > aside.doctor").appendChild(main);
         }
     }
+
+    function filter_hospital() {
+        self.scrollTop = 0;
+        current_data_filter_hospital = [];
+        render_hospital();
+    }
+
+    function render_hospital() {
+        self.querySelectorAll(":scope > aside.hospital > main").remove();
+        for (var i = 0; i < current_data_filter_hospital.length; i++) {
+            holder.innerHTML = self.querySelector(":scope > aside.hospital > template").innerHTML;
+            var main = holder.firstElementChild;
+            if (current_data_filter_hospital[i].image && current_data_filter_hospital[i].image.id) {
+                main.querySelector(":scope > nav > svg").style.backgroundImage = "url('" + Api.Storage(current_data_filter_hospital[i].image.id, current_data_filter_hospital[i].image.extension) + "')";
+            }
+            main.querySelector(":scope > header > section.address > div.address").innerHTML = current_data_filter_hospital[i].address;
+            main.querySelector(":scope > header > section.address > div.address2").innerHTML = current_data_filter_hospital[i].address2;
+            main.querySelector(":scope > header > section.address > div.city").innerHTML = current_data_filter_hospital[i].city;
+            main.querySelector(":scope > header > section.address > div.state").innerHTML = current_data_filter_hospital[i].state + " " + current_data_filter_hospital[i].zipcode;
+            main.querySelector(":scope > header > section.description").innerHTML = current_data_filter_hospital[i].description;
+            for (var service_id in current_data_filter_hospital[i].services) {
+                var service = Cache.Hash("service", current_data_filter_hospital[i].services[service_id].id);
+                var div = document.createElement("div");
+                div.innerHTML = service.name["zh-Hans"] + " $" + current_data_filter_hospital[i].services[service_id].money;
+                main.querySelector(":scope > footer > section.services").appendChild(div);
+            }
+            main.querySelector(":scope > header > section.name").innerHTML = current_data_filter_hospital[i].name.en;
+            main.querySelector(":scope > a").href = "#l=hospital&id=" + current_data_filter_hospital[i].id;
+            self.querySelector(":scope > aside.hospital").appendChild(main);
+        }
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        current_latitude = position.coords.latitude;
+        current_longitude = position.coords.longitude;
+    });
 
 }());
