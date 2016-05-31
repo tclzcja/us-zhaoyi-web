3f258675f0a99e6a551b616e3563567eb696bc43
JingAn Chen <lzsoft.cja@gmail.com>
2016-05-30 22:37:53 -0400

    Temp.

--- js/page/doctor.js
+++ js/page/doctor.js
@@ -1,4 +1,4 @@
-/* jshint browser: true, esversion: 6, devel: true, sub: true */
+/* jshint browser: true, esversion: 6, devel: true, sub: true, shadow: true, loopfunc: true */
 
 (function () {
 
@@ -13,6 +13,8 @@
     var map_subject = new Map();
     var map_service = new Map();
     var map_insurance = new Map();
+
+    var comment_index = 0;
 
     function load(callback) {
 
@@ -91,15 +93,73 @@
         document.querySelector("body > main.info > table > tbody > tr > td.phone > div").innerHTML = doctor.phone;
 
         // Star
-        var star = doctor.star.total / doctor.star.amount || 1;
-        for (var i = 1; i <= 5; i++) {
-            if (star >= i) {
-                document.querySelector("body > main.comment > table > tbody > tr > td.star > footer").innerHTML += document.querySelector("body > main.comment > table > tbody > tr > td.star > footer > template.star").innerHTML;
-            } else {
-                document.querySelector("body > main.comment > table > tbody > tr > td.star > footer").innerHTML += document.querySelector("body > main.comment > table > tbody > tr > td.star > footer > template.star-empty").innerHTML;
+        if (doctor.star.amount === 0) {
+            document.querySelectorAll("body > main.comment > table > tbody > tr > td").setAttribute("data-empty", true);
+        } else {
+            var star = doctor.star.total / doctor.star.amount;
+            document.querySelector("body > main.comment > table > tbody > tr > td.star > div").innerHTML = star.toFixed(1) + '<span>/5.0</span>';
+            for (var i = 1; i <= 5; i++) {
+                if (star >= i) {
+                    document.querySelector("body > main.comment > table > tbody > tr > td.star > footer").innerHTML += document.querySelector("body > main.comment > table > tbody > tr > td.star > footer > template.star").innerHTML;
+                } else {
+                    document.querySelector("body > main.comment > table > tbody > tr > td.star > footer").innerHTML += document.querySelector("body > main.comment > table > tbody > tr > td.star > footer > template.star-empty").innerHTML;
+                }
             }
+            Api.Core("/comment/read", {
+                doctor_id: doctor._id
+            }, function () {
+
+            });
         }
-        document.querySelector("body > main.comment > table > tbody > tr > td.star > div").innerHTML = star.toFixed(1) + '<span>/5.0</span>';
+
+        // Schedule
+        for (var i = 0; i < doctor.schedule.length; i++) {
+            Api.Core("/hospital/read", {
+                _id: doctor.schedule[i].hospital_id
+            }, function (hospital) {
+                console.log(hospital[0]);
+                var main = document.createElement("div");
+                main.innerHTML = document.querySelector("body > main.schedule > template").innerHTML;
+                main = main.firstElementChild;
+                document.querySelector("body > main.schedule").appendChild(main);
+                var table = main.querySelector(":scope > table");
+
+                // Portrait
+                if (hospital[0].portrait) {
+                    table.querySelector(":scope > tbody > tr > td.portrait").style.backgroundImage = 'url("' + Api.Storage(hospital[0].portrait._id, hospital[0].portrait.extension) + '")';
+                }
+
+                // Name
+                table.querySelector(":scope > tbody > tr > td.name > div").innerHTML = hospital[0].name["en"];
+
+                // Description
+                table.querySelector(":scope > tbody > tr > td.description > div").innerHTML = hospital[0].description;
+
+                // Address
+                table.querySelector(":scope > tbody > tr > td.address > div").innerHTML = hospital[0].address;
+
+                // Address 2
+                table.querySelector(":scope > tbody > tr > td.address2 > div").innerHTML = hospital[0].address2;
+
+                // City
+                table.querySelector(":scope > tbody > tr > td.city > div").innerHTML = hospital[0].city;
+
+                // State
+                table.querySelector(":scope > tbody > tr > td.state > div").innerHTML = hospital[0].state;
+
+                // Zipcode
+                table.querySelector(":scope > tbody > tr > td.zipcode > div").innerHTML = hospital[0].zipcode;
+
+                // Iframe
+                if (hospital[0].iframe) {
+                    main.querySelector(":scope > header").innerHTML = hospital[0].iframe;
+                }
+            });
+        }
+    }
+
+    function fetch_comment() {
+
     }
 
     load(render);
