6a95db65afd22868365a249ffcfde8291a646e4f
JingAn Chen <lzsoft.cja@gmail.com>
2016-06-14 15:57:58 -0400

    Done.

--- js/page/hospital.js
+++ js/page/hospital.js
@@ -1,59 +1,82 @@
-/* jshint browser: true, esnext: true, devel: true, shadow: true */
+/* jshint browser: true, esversion: 6, devel: true, sub: true, shadow: true, loopfunc: true */
 
 (function () {
 
     'use strict';
 
-    var self = document.querySelector("body > #hospital");
-    var holder = document.createElement("div");
-
     var Api = window.Api;
     var Auth = window.Auth;
-    var Cache = window.Cache;
     var Param = window.Param;
 
-    self.addEventListener("hey", render);
+    var hospital = {};
+    var map_service = new Map();
+
+    var holder = document.createElement("div");
+
+    function load() {
+
+        var fire_click = 0;
+
+        Api.Core("/hospital/read", {
+            _id: Param.Get("_id")
+        }, function (data) {
+            hospital = data[0];
+            fire();
+        });
+
+        Api.Core("/service/read", null, function (data) {
+            for (var i = 0; i < data.length; i++) {
+                map_service.set(data[i]._id, data[i]);
+            }
+            fire();
+        });
+
+        function fire() {
+            fire_click++;
+            if (fire_click >= 2) {
+                render();
+            }
+        }
+
+    }
 
     function render() {
 
-        self.scrollTop = 0;
-        self.querySelector(":scope > header > main > header > svg").removeAttribute("style");
-        self.querySelector(":scope > header > main > section.name").innerHTML = "";
-        self.querySelector(":scope > header > main > section.address").innerHTML = "";
-        self.querySelector(":scope > header > main > section.address2").innerHTML = "";
-        self.querySelector(":scope > header > main > section.city").innerHTML = "";
-        self.querySelector(":scope > header > main > section.state").innerHTML = "";
-        self.querySelector(":scope > header > main > section.zipcode").innerHTML = "";
-        self.querySelector(":scope > main > main > section.iframe").innerHTML = "没有地图数据";
-        self.querySelector(":scope > footer > main > section.services > table > tbody").innerHTML = "";
+        // Portrait
+        if (hospital.portrait) {
+            document.querySelector("body > main.info > table > tbody > tr > td.portrait").style.backgroundImage = 'url("' + Api.Storage(hospital.portrait._id, hospital.portrait.extension) + '")';
+        }
 
-        Api.Core("hospital", "single", {
-            id: Param.Get("id")
-        }, function (data) {
-            if (data.image.id) {
-                self.querySelector(":scope > header > main > header > svg").style.backgroundImage = "url(" + Api.Storage(data.image.id, data.image.extension) + ")";
-            }
-            self.querySelector(":scope > header > main > section.name").innerHTML = data.name.en;
-            self.querySelector(":scope > header > main > section.address").innerHTML = data.address;
-            self.querySelector(":scope > header > main > section.address2").innerHTML = data.address2;
-            self.querySelector(":scope > header > main > section.city").innerHTML = data.city;
-            self.querySelector(":scope > header > main > section.state").innerHTML = data.state;
-            self.querySelector(":scope > header > main > section.zipcode").innerHTML = data.zipcode;
-            if (data.iframe) {
-                self.querySelector(":scope > main > main > section.iframe").innerHTML = data.iframe;
-            }
-            for (var i = 0; i < data.services.length; i++) {
-                var tr = document.createElement("tr");
-                var td1 = document.createElement("td");
-                td1.innerHTML = Cache.Hash("service", data.services[i].service_id).name["zh-Hans"];
-                var td2 = document.createElement("td");
-                td2.innerHTML = "$" + data.services[i].money;
-                tr.appendChild(td1);
-                tr.appendChild(td2);
-                self.querySelector(":scope > footer > main > section.services > table > tbody").appendChild(tr);
-            }
-        });
+        // Name
+        document.querySelector("body > main.info > table > tbody > tr > td.name > div").innerHTML = "Dr. " + hospital.name["en"];
 
+        // Description
+        document.querySelector("body > main.info > table > tbody > tr > td.description > div").innerHTML = hospital.description;
+
+        // Address
+        document.querySelector("body > main.info > table > tbody > tr > td.address > div").innerHTML = hospital.address;
+
+        // Address2
+        document.querySelector("body > main.info > table > tbody > tr > td.address2 > div").innerHTML = hospital.address2;
+
+        // City
+        document.querySelector("body > main.info > table > tbody > tr > td.city > div").innerHTML = hospital.city;
+
+        // Zipcode
+        document.querySelector("body > main.info > table > tbody > tr > td.zipcode > div").innerHTML = hospital.zipcode;
+
+        // State
+        document.querySelector("body > main.info > table > tbody > tr > td.state > div").innerHTML = hospital.state;
+
+        // Service List
+        for (var service of hospital.service_list) {
+            var span = document.createElement("span");
+            span.innerHTML = map_service.get(service.service_id).name["zh-Hans"] + " " + map_service.get(service.service_id).name["en"];
+            span.setAttribute("data-price", "$" + service.price);
+            document.querySelector("body > main.info > table > tbody > tr > td.service").appendChild(span);
+        }
     }
+
+    load();
 
 }());
