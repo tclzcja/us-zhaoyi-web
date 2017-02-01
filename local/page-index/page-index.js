abb295003c76e19a2ecfc26f4451c73652ad05c6
JingAn Chen <lzsoft.cja@gmail.com>
2017-02-01 18:51:47 -0500

    Temp for Index.

--- local/page-index/page-index.js
+++ local/page-index/page-index.js
@@ -2,8 +2,13 @@ window.customElements.define('page-index', class extends HTMLElement {
     constructor() {
         super();
         this.innerHTML = document.querySelector('link[href*="page-index.html"]').import.querySelector("template").innerHTML;
-        setTimeout(function() {
-            document.body.classList.add("bg");
-        }, 1000);
+        this.eventize();
+    }
+    eventize() {
+        let self = this;
+        self.querySelector("select").addEventListener("change", function() {
+            self.querySelector(":scope > main").classList.remove("doctor", "hospital", "service");
+            self.querySelector(":scope > main").classList.add(this.value);
+        });
     }
 });
