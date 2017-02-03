7a9d837cdc868904377632b8325c77fed002e716
JingAn Chen <lzsoft.cja@gmail.com>
2017-02-03 18:28:35 -0500

    Temp.

--- local/page-search/page-search.js
+++ local/page-search/page-search.js
@@ -3,6 +3,17 @@ window.customElements.define('page-search', class extends HTMLElement {
         super();
         window.Lzsoft.Import.ByTagImport(this);
         this.eventize();
+        this.init();
+    }
+    init() {
+        let self = this;
+        let t = window.location.GetParam("t");
+        let k = window.location.GetParam("k");
+        if (t && k) {} else {
+            setTimeout(function() {
+                self.querySelector(":scope > single-page-application-data > header").classList.add("on");
+            }, 500);
+        }
     }
     eventize() {
         let self = this;
